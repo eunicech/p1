04365b58fdee474bcfb377f0ca1dd044fbe3282d
Nikhita Subbiah <nikkisubbiah@users.noreply.github.com>
2020-10-08 23:56:19 -0400

    epoch retries

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -3,8 +3,10 @@
 package lsp
 
 import (
+	"container/list"
 	"encoding/json"
 	"errors"
+	"time"
 
 	"github.com/cmu440/lspnet"
 )
@@ -12,11 +14,12 @@ import (
 type client struct {
 	//epoch          int
 	//epochLimit       int
-	//windowSize       int
+	windowSize int
 	//maxUnackedMsgs   int
-	//backOff          int
-	//maxBackOff       int
-	//timer            *time.Ticker
+	maxBackOff    int
+	epochSize     int
+	ticker        *time.Ticker
+	timer         *time.Time
 	clientID      int
 	conn          *lspnet.UDPConn
 	currSN        int      //keeps track of sequence numbers
@@ -24,13 +27,15 @@ type client struct {
 	server_sn_res chan int // get what packet # we are waiting for
 	get_server_sn chan bool
 	client_sn_res chan int // get what packet # we are writing
-	get_client_sn chan int
-	//pendingMsgs    *list.List
-	pendingMsgChan chan *Message
-	acks           chan Message
-	dataStorage    data
-	closeActivate  chan bool
-	closed         bool
+	get_client_sn chan bool
+	window        *slidingWindow
+	acks          chan Message
+	dataStorage   data
+	closeActivate chan bool
+	closed        bool
+	wroteInEpoch  bool
+	writtenChan   chan bool
+	signalEpoch   chan bool
 }
 
 type data struct {
@@ -43,6 +48,20 @@ type data struct {
 type readReq struct {
 	dataSN  int
 	dataRes chan Message
+}
+
+type slidingWindow struct {
+	pendingMsgs    *list.List
+	pendingMsgChan chan []byte
+	start          int
+}
+
+type windowElem struct {
+	sn          int
+	ackChan     chan Message
+	signalEpoch chan bool
+	msg         *Message
+	gotAck      bool
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -65,58 +84,110 @@ func NewClient(hostport string, params *Params) (Client, error) {
 		return nil, err
 	}
 
-	//send connection
-	msg, err := json.Marshal(NewConnect())
-	if err != nil {
-		return nil, err
-	}
-
-	udp.Write(msg)
-	// wait for acknowledgement
-	var ack_msg Message
-	var ack [2000]byte
-	bytesRead, err := udp.Read(ack[0:])
-	if err != nil {
-		return nil, err
-	}
-	json.Unmarshal(ack[:bytesRead], &ack_msg)
-	if ack_msg.Type != MsgAck || ack_msg.SeqNum != 0 {
-		return nil, errors.New("Not an acknowledgement")
-	}
-
 	dataStore := data{
 		read_reqs:   make(chan readReq),
 		pendingData: make(map[int]Message),
 		addData:     make(chan Message),
 		closed:      make(chan bool),
 	}
-
+	window := &slidingWindow{
+		start:          1,
+		pendingMsgs:    list.New(),
+		pendingMsgChan: make(chan []byte),
+	}
 	new_client := &client{
 		//epochLimit:     params.EpochLimit,
-		//windowSize:     params.WindowSize,
-		//maxBackOff:     params.MaxBackOffInterval,
+		windowSize: params.WindowSize,
+		maxBackOff: params.MaxBackOffInterval,
 		//maxUnackedMsgs: params.MaxUnackedMessages,
-		//timer:          time.NewTicker(time.Duration(1000000 * params.EpochMillis)),
-		clientID:      ack_msg.ConnID,
+		writtenChan: make(chan bool),
+		epochSize:   params.EpochMillis,
+		ticker:      time.NewTicker(time.Duration(1000000 * params.EpochMillis)),
+		//clientID:      ack_msg.ConnID,
 		conn:          udp,
 		currSN:        1,
 		serverSN:      1,
 		server_sn_res: make(chan int),
 		get_server_sn: make(chan bool),
 		client_sn_res: make(chan int),
-		get_client_sn: make(chan int),
-		//pendingMsgs:    list.New(),
-		pendingMsgChan: make(chan *Message),
-		acks:           make(chan Message),
-		dataStorage:    dataStore,
-		closeActivate:  make(chan bool),
-		closed:         false,
+		get_client_sn: make(chan bool),
+		window:        window,
+		acks:          make(chan Message),
+		dataStorage:   dataStore,
+		closeActivate: make(chan bool),
+		closed:        false,
+		signalEpoch:   make(chan bool),
 	}
+
+	var epochsPassed int = 0
+	var currentBackOff int = 0
+	//send connection
+	msg, err := json.Marshal(NewConnect())
+	if err != nil {
+		return nil, err
+	}
+	udp.Write(msg)
+
+	ack_chan := make(chan Message)
+	err_chan := make(chan error)
+	var ack_msg Message
+	go readConnection(ack_chan, udp, err_chan)
+
+	var flag bool = false
+	for {
+		select {
+		case <-new_client.ticker.C:
+			epochsPassed += 1
+			if epochsPassed > currentBackOff {
+				udp.Write(msg)
+				epochsPassed = 0
+				if currentBackOff == 0 {
+					currentBackOff = 1
+				} else {
+					currentBackOff = currentBackOff * 2
+				}
+			}
+		case ack := <-ack_chan:
+			ack_msg = ack
+			flag = true
+			break
+		case newErr := <-err_chan:
+			err = newErr
+			flag = true
+			break
+		}
+		if flag {
+			break
+		}
+
+	}
+	if err != nil {
+		return nil, err
+	}
+	new_client.clientID = ack_msg.ConnID
 	go new_client.clientInfoRequests()
 	go new_client.writeRoutine()
 	go new_client.readRoutine()
 	go new_client.readRequestsRoutine()
 	return new_client, nil
+}
+
+func readConnection(ack_chan chan Message, udp *lspnet.UDPConn, err_chan chan error) {
+	for {
+		var ack_msg Message
+		var ack [2000]byte
+		bytesRead, err := udp.Read(ack[0:])
+		if err != nil {
+			err_chan <- err
+			break
+		}
+		json.Unmarshal(ack[:bytesRead], &ack_msg)
+		if ack_msg.Type == MsgAck && ack_msg.SeqNum == 0 {
+			ack_chan <- ack_msg
+			break
+		}
+	}
+
 }
 
 func (c *client) clientInfoRequests() {
@@ -125,17 +196,23 @@ func (c *client) clientInfoRequests() {
 		case <-c.get_server_sn:
 			c.server_sn_res <- c.serverSN
 			c.serverSN += 1
-		case add := <-c.get_client_sn:
-			if add < 0 {
-				//this is a request to get the currSN
-				c.client_sn_res <- c.currSN
-			} else {
-				//this is request to add to currSN
-				c.currSN += add
-			}
+		case <-c.get_client_sn:
+			c.client_sn_res <- c.currSN
+			c.currSN += 1
 		case <-c.closeActivate:
 			c.closed = true
 			break
+		case <-c.ticker.C:
+			if !c.wroteInEpoch {
+				//send heartbeat
+				ack_msg := NewAck(c.clientID, 0)
+				byte_msg, _ := json.Marshal(&ack_msg)
+				c.conn.Write(byte_msg)
+				c.wroteInEpoch = false
+				c.signalEpoch <- true
+			}
+		case <-c.writtenChan:
+			c.wroteInEpoch = true
 		}
 	}
 }
@@ -200,11 +277,28 @@ func (c *client) readRoutine() {
 
 }
 
-func (c *client) writeMsg(msg Message) {
+func (c *client) writeMsg(msg Message, ackChan chan Message, sigEpoch chan bool) {
 	byte_msg, _ := json.Marshal(&msg)
+	var currentBackOff int = 0
+	var epochsPassed int = 0
 	c.conn.Write(byte_msg)
-	//wait for acknowledgement
-	<-c.acks
+	for {
+		select {
+		case <-ackChan:
+			break
+		case <-sigEpoch:
+			epochsPassed += 1
+			if epochsPassed > currentBackOff {
+				c.conn.Write(byte_msg)
+				epochsPassed = 0
+				if currentBackOff == 0 {
+					currentBackOff = 1
+				} else {
+					currentBackOff = currentBackOff * 2
+				}
+			}
+		}
+	}
 }
 
 func (c *client) writeRoutine() {
@@ -214,8 +308,66 @@ func (c *client) writeRoutine() {
 		case <-c.closeActivate:
 			close(c.dataStorage.closed)
 			break
-		case dataMsg := <-c.pendingMsgChan:
-			go c.writeMsg(*dataMsg)
+		case payload := <-c.window.pendingMsgChan:
+			c.get_client_sn <- true
+			sn := <-c.client_sn_res
+			checksum := uint16(ByteArray2Checksum(payload))
+			dataMsg := NewData(c.clientID, sn, len(payload), payload, checksum)
+			ackChan := make(chan Message)
+			newSignalEpoch := make(chan bool)
+			newElem := &windowElem{
+				sn:          sn,
+				msg:         dataMsg,
+				ackChan:     ackChan,
+				gotAck:      false,
+				signalEpoch: newSignalEpoch,
+			}
+			c.window.pendingMsgs.PushBack(newElem)
+			if c.window.pendingMsgs.Len() == 1 {
+				go c.writeMsg(*dataMsg, ackChan, newSignalEpoch)
+			}
+		case <-c.signalEpoch:
+			//signal epochs to all messages in window
+			count := 0
+			// for elem := c.window.pendingMsgs.Front(); elem != nil && count < c.windowSize; elem = elem.Next() {
+			for elem := c.window.pendingMsgs.Front(); elem != nil && count < 1; elem = elem.Next() {
+				currElem := elem.Value.(*windowElem)
+				if !currElem.gotAck {
+					currElem.signalEpoch <- true
+				}
+				count += 1
+			}
+		case ack := <-c.acks:
+			//put ack in corresponding channel
+			sn := ack.SeqNum
+			for elem := c.window.pendingMsgs.Front(); elem != nil; elem = elem.Next() {
+				currElem := elem.Value.(*windowElem)
+				if currElem.sn == sn {
+					currElem.ackChan <- ack
+					currElem.gotAck = true
+				}
+			}
+			//remove messages that are good
+			var keepRemoving bool = true
+			currElem := c.window.pendingMsgs.Front()
+			for keepRemoving && c.window.pendingMsgs.Len() > 0 {
+				wElem := currElem.Value.(*windowElem)
+				if wElem.gotAck {
+					c.window.pendingMsgs.Remove(currElem)
+					currElem = c.window.pendingMsgs.Front()
+				} else {
+					keepRemoving = false
+				}
+			}
+
+			// TODO: sliding window only one rn
+			//update window
+			if c.window.pendingMsgs.Len() > 0 {
+				newElem := c.window.pendingMsgs.Front().Value.(*windowElem)
+				c.window.start = newElem.sn
+				//spawn new goroutines
+				go c.writeMsg(*newElem.msg, newElem.ackChan, newElem.signalEpoch)
+			}
 		}
 
 	}
@@ -239,12 +391,7 @@ func (c *client) Read() ([]byte, error) {
 }
 
 func (c *client) Write(payload []byte) error {
-	c.get_client_sn <- -1
-	sn := <-c.client_sn_res
-	checksum := uint16(ByteArray2Checksum(payload))
-	dataMsg := NewData(c.clientID, sn, len(payload), payload, checksum)
-	c.pendingMsgChan <- dataMsg
-	c.get_client_sn <- 1
+	c.window.pendingMsgChan <- payload
 	return nil
 }
 
